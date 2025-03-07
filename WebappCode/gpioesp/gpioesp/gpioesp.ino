aaf0746a57c5795ce455c42b12cc4dcb9ef662f8
Agasthya <122645271+AgasthyaV@users.noreply.github.com>
2025-03-06 17:12:06 -0800

    Agasthya gpiomerge

--- WebappCode/gpioesp/gpioesp/gpioesp.ino
+++ WebappCode/gpioesp/gpioesp/gpioesp.ino
@@ -1,4 +1,4 @@
-// #include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <MightyZap.h>
 
@@ -8,21 +8,20 @@
 #define MAX_POS 4095  // Default max position
 #define TXD1 18
 #define RXD1 19
-#define I2C_SDA 34
-#define I2C_SCL 25
 #define MIGHTY_ZAP_RX 16  // Define RX pin for Serial2
 #define MIGHTY_ZAP_TX 17  // Define TX pin for Serial2
 #define MIGHTY_ZAP_EN 13  // Enable pin
-#define ANALOG_PIN 34
-#define ZERO_SAMPLES 50
-#define COUNTS_PER_PSI 735990  // Increased to further scale down PSI values
-#define PSI_TO_CMH2O 70.307    // Conversion factor from PSI to cmH2O
-#define DRIFT_THRESHOLD 0.1    // PSI threshold to consider as "no pressure"
-#define STABLE_TIME 3000       // Time in ms that readings need to be stable for re-zeroing
-#define DRIFT_SAMPLES 10       // Number of samples to check for drift
 
 HardwareSerial mySerial(1);
 
+uint8_t data[7];
+uint8_t cmd[3] = { 0xAA, 0x00, 0x00 };
+double press_counts = 0;
+double pressure = 0;
+double outputmax = 15099494;
+double outputmin = 1677722;
+double pmax = 1;
+double pmin = 0;
 float minpressure;
 float maxpressure;
 float attacktime;
@@ -33,8 +32,7 @@ float susttime;
 
 int lcdColumns = 20;
 int lcdRows = 4;
-//LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
-String success;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 int sustaintime = 1000;
 float frequency = 0.75;            // Default frequency in Hz
 unsigned long lastSensorRead = 0;  // For timing sensor readings
@@ -49,7 +47,7 @@ int calibration(int calibrationstate) {
   int calstarttime = 0;
   int reached = 0;
   while (calibrationstate == 1) {
-    Serial.printf("MODEL NUM:%d\n",m_zap.getModelNumber(ID_NUM));
+    Serial.printf("MODEL NUM:%d\n", m_zap.getModelNumber(ID_NUM));
     int curpos = m_zap.presentPosition(ID_NUM);
     if (reached == 0) {
       if (abs(curpos - MIN_POS) < 3) {
@@ -77,17 +75,7 @@ int calibration(int calibrationstate) {
     }
   }
 }
-float maptopsi(int value) {
-  if (value < -16383) value = -16383;
-  if (value > 16383) value = 16383;
-  // Perform the mapping:
-  // We need to scale from a 32766 range (-16383 to 16383) to a 200 range (-100 to 100)
-  float result = (value / 16383.0) * 100.0;
-  //Serial.println(result);
-  result = result * 0.0145;
-  return result;
-}
-//HardwareSerial Serial2(2);
+
 
 int calculateSpeed(int distance, float freq) {
   // Total period = 1/frequency
@@ -113,41 +101,32 @@ void updateParameters() {
 }
 
 String readPressureRaw() {
-  int pressure_raw = 0;
-  Wire.requestFrom(I2C_ADDRESS, 2);
-  if (Wire.available() >= 2) {
-    uint8_t highByte = Wire.read();
-    uint8_t lowByte = Wire.read();
-    pressure_raw = (highByte << 8) | lowByte;
-  } else {
-    //Serial.println("No data received.");
+  Wire.beginTransmission(I2C_ADDRESS);
+  int stat = Wire.write(cmd, 3);
+  stat |= Wire.endTransmission();
+  delay(10);
+  int i = 0;
+  Wire.requestFrom(I2C_ADDRESS, 7);
+  for (i = 0; i < 7; i++) {
+    data[i] = Wire.read();
   }
-  float psi = maptopsi(pressure_raw);
+  press_counts = data[3] + data[2] * 256 + data[1] * 65536;  // calculate digital pressure counts
+  pressure = ((press_counts - outputmin) * (pmax - pmin)) / (outputmax - outputmin) + pmin;
+  String psi = String(pressure, 3);
 
-  //Serial.println(psi);
-  String pressure = String(psi, 3);
-
-  return pressure;
+  return psi;
 }
 
 void setup() {
 
   Serial.begin(115200);
-  Wire.begin(I2C_SDA,I2C_SCL);
+  Wire.begin();
   Serial2.begin(32, SERIAL_8N1, MIGHTY_ZAP_RX, MIGHTY_ZAP_TX);
   m_zap.begin(32);
   Wire.beginTransmission(I2C_ADDRESS);
   mySerial.begin(115200, SERIAL_8N1, RXD1, TXD1);
-  analogSetWidth(12);
-  cycleStartTime = millis();
 
-  if (Wire.endTransmission() == 0) {
-    Serial.println("Pressure sensor found at 0x28.");
-  } else {
-    Serial.println("Pressure sensor not found! Check wiring.");
-  }
-
-  /* lcd.init();
+  lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Press. |");
@@ -172,12 +151,11 @@ void setup() {
   lcd.setCursor(10, 3);
   lcd.print("Hz");
   lcd.setCursor(17, 3);
-  lcd.print("s"); */
+  lcd.print("s"); 
 }
 
 void loop() {
   String pres = readPressureRaw();
-  Serial.println(pres);
   float totalPeriod = 1000.0 / frequency;             // Period in milliseconds
   float activeTime = totalPeriod * DUTY_CYCLE;        // Active time in milliseconds
   float restTime = totalPeriod * (1.0 - DUTY_CYCLE);  // Rest time in milliseconds
@@ -215,7 +193,7 @@ void loop() {
     calibrationstate = calibration(calibrationstate);
   } */
 
-  /* if (currentTime < activeTime) {
+  if (currentTime < activeTime) {
     // First half of active period: forward movement
     if (currentTime < activeTime / 2) {
       m_zap.GoalPosition(ID_NUM, 2000);
@@ -233,7 +211,7 @@ void loop() {
     if (!m_zap.Moving(ID_NUM)) {
       delay(10);  // Small delay to prevent CPU hogging
     }
-  } */
+  } 
   // Continue sensor readings during movement
   while (m_zap.Moving(ID_NUM)) {
     if (millis() - lastSensorRead >= 25) {
